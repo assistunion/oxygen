7f4de81a08c4b811657f12bfe96e7913fc7661dc
Anatoly Ressin <aar.now@gmail.com>
2012-03-27 14:34:01 +0300

    Added Oxygen_Lib::url() and fixed static for Oxygen_Lib::path

--- lib/lib.class.php
+++ lib/lib.class.php
@@ -1,8 +1,15 @@
 <?
 
     class Oxygen_Lib {
-        public function path($path) {
+        public static function path($path) {
             return dirname(__file__) . DIRECTORY_SEPARATOR . $path;
+        }
+
+        public static function url($path) {
+        	$document_root = str_replace('/',DIRECTORY_SEPARATOR, $_SERVER['DOCUMENT_ROOT']);
+        	$full_path = dirname(__file__) . DIRECTORY_SEPARATOR . $path;
+        	$url = str_replace(DIRECTORY_SEPARATOR, '/', str_replace($document_root, '', $full_path));
+        	return $url;
         }
     }
 
