a39db8f41ebaefe704aca7820bc64a72d54a1770
Anatoly Ressin <aar.now@gmail.com>
2012-04-03 16:06:59 +0300

    Compiling routes

--- controller/controller.class.php
+++ controller/controller.class.php
@@ -4,17 +4,26 @@
 		implements ArrayAccess, IteratorAggregate, Countable
 	{
 
-		const INVALID_CLASS_RETRIEVER = 'Invalid class retriever';
+		const TYPES_REGEXP = '/^(?:(int)|(str)|\/([^\/]*(?:\\\\\/[^\/]*)*)\/)$/';
+		const PARAM_REGEXP = '/{([0-9A-Za-z_]+):([^{}]+)}/';
 
-		private $visualChild = null;
-		private $logicalChild = null;
-		private $visualParent = null;
+		const PARAM_GUARD_REPLACE = '#\\1#';
+		const PARAM_GUARD_REGEXP  = '/#([0-9A-Za-z_]+)#/e';
+
+		const INT_REGEXP_BARE = '[0-9]+';
+		const STR_REGEXP_BARE = '[^/]+';
+
+
+		const INVALID_CLASS_RETRIEVER = 'Invalid class retriever';
+		const ROUTE_PARAM_REDEFINED   = 'Route param redefined';
+		const INVALID_PARAM_TYPE      = 'Invalid route parameter type';
+
+		private $visualChild   = null;
+		private $logicalChild  = null;
+		private $visualParent  = null;
 		private $logicalParent = null;
-		private $model = null;
-		private $configured = false;
-		private $children = array();
-		private $childDefs = array();
-		private $colDefs = array();
+		private $model         = null;
+		private $configured    = false;
 
 		public function __construct($model){
 			$this->model = $model;
@@ -65,15 +74,59 @@
 			}
 		}
 
-		public static function compileRoute($route){
+		private function getRegexpFor ($type){
+			if(preg_match(self::TYPES_REGEXP, $type,$match)){
+				switch (count($match)) {
+					case 2: return self::INT_REGEXP_BARE;
+					case 3: return self::STR_REGEXP_BARE;
+					case 4: return $match[3];
+				}
+			} else {
+				$this->throwException(self::INVALID_PARAM_TYPE);
+			}
+		}
+
+
+		public function compileRoute($route){
 			$route = trim($route,'/');
 			$specificy = substr_count('/', $route) + 1;
-			preg_match_all('/{([0-9A-Za-z_]+:(?int|name))}/', $route, $match);
+			if(0 < preg_match_all(self::PARAM_REGEXP, $route, $match)){
+				$names = $match[1];
+				$types = $match[2];
+				$params = array();
+				foreach($names as $i => $name){
+					if(isset($params[$name])) {
+						$this->throwException(self::ROUTE_PARAM_REDEFINED);
+					} else {
+						$params[$name] = self::getRegexpFor($types[$i]);
+					}
+				}
+				$cpl = preg_replace(self::PARAM_REGEXP,self::PARAM_GUARD_REPLACE, $route);
+				$cpl = preg_quote($cpl,'/');
+				$cpl = preg_replace(self::PARAM_GUARD_REGEXP,"'(?P<\\1>'.\$params[\\1].')'", $cpl);
+				return $cpl;
+			} else {
+			}
 			return $match;
 		}
 
+		private function postConfigure() {
+
+		}
+
+		public function ensureConfigured() {
+			if(!$this->configured){
+				$this->configure();
+			    $this->postConfigure();
+			}
+		}
+
 		public function add($class, $route, $model, $iterable) {
-			$this['x-{id:int}/{a:ident}']->Partner($model);
+			if(!$this->configured) {
+
+			} else {
+
+			}
 		}
 
 		public function configure() {
