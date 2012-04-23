b5e38acb8e43f212000b65df18be7c89290a610a
Anatoly Ressin <aar.now@gmail.com>
2012-04-23 06:45:55 +0300

    Added ability to specify additional parameters for controller configs

--- controller/configurator/configurator.class.php
+++ controller/configurator/configurator.class.php
@@ -7,9 +7,21 @@
 			$this->controller = $controller;
 			$this->route = $route;
 		}
+		private $factory = null;
+		private $args = null;
+
+		public function getWrapped($model) {
+			$args = $this->args;
+			$args[0] = $model;
+			return $this->factory->getInstance($args);
+		}
+
 		public function __call($class, $args) {
+			$this->factory = $this->controller->scope->resolve($class);
 			$model = isset($args[0])?$args[0]:null;
-			return $this->controller->add($class,$this->route,$model);
+			$args[0] = null; // Erase model info;
+			$this->args = $args;
+			return $this->controller->add(array($this,'getWrapped'),$this->route,$model);
 		}
 	}
 
