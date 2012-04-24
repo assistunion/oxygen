af51e101a0dcad83502fe2734b07d7ccb00dab34
Anatoly Ressin <aar.now@gmail.com>
2012-04-24 15:16:49 +0300

    Fixed callable

--- factory/callable/callable.class.php
+++ factory/callable/callable.class.php
@@ -2,17 +2,10 @@
 
     class Oxygen_Factory_Callable extends Oxygen_Factory {
         public function getInstance($args = array(), $scope = null) {
-            if ($scope = null) $scope = $this->getScope();
+            if ($scope === null) $scope = $this->scope;
             $callable = $this->getDefinition();
-            switch(count($args)){
-            case 0: $result = $callable($scope); break;
-            case 1: $result = $callable($scope,$args[0]); break;
-            case 2: $result = $callable($scope,$args[0], $args[1]); break;
-            case 3: $result = $callable($scope,$args[0], $args[1], $args[2]); break;
-            default:
-                array_unshift($args, $scope);
-                $result = call_user_func_array($callable, $args);
-            }
+            array_unshift($args, $scope);
+            return call_user_func_array($callable, $args);
         }
     }
 
