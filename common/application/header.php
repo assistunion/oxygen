c2dab065fc6f392d17614c6535d1679c4c01cce2
Anatoly Ressin <aar.now@gmail.com>
2012-05-31 05:29:33 +0300

    Header cleanup

--- common/application/header.php
+++ common/application/header.php
@@ -2,19 +2,12 @@
 <?$this->put_logo()?>
 <?$current = $this->getCurrent()?>
 <h1><?$current->put_title()?></h1>
+<?$this->put_login()?>
 <ul class="bread-crumbs">
 <?$breadCrumbs = $this->getPathToCurrent()?>
 <?array_pop($breadCrumbs)?>
 <?foreach($breadCrumbs as $child):?>
-<li><a href="<?=$child->go()?>"><?=$child?></a><?/*
-	<?if($child):?>
-	<ul class="submenu">
-		<?foreach($child as $ch):?>
-		<li><?$ch->put_as_child()?></li>
-		<?endforeach?>
-	</ul>
-	<?endif?>*/?>
-</li>
+<li><a href="<?=$child->go()?>"><?=$child?></a></li>
 <?endforeach?>
 </ul>
-<?$this->put_login()?>
+
